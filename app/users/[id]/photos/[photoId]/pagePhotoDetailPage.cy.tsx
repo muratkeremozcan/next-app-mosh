e05841cf5bcb2bd730c9821d0dce5a8774fd0b41
Murat Ozcan <muratkerem@gmail.com>
2023-09-29 09:23:02 -0500

    routing

---
+++ app/users/[id]/photos/[photoId]/pagePhotoDetailPage.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PhotoDetailPage from './page'
+
+describe('<PhotoDetailPage />', () => {
+  it('renders', () => {
+    cy.mount(<PhotoDetailPage params={{id: 42, photoId: 84}} />)
+    cy.contains('User 42, PhotoDetailPage 84')
+  })
+})
