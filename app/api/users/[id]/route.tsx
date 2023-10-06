eb807e46e4035ddc6452cca1b2c0e5811c730d6e
Murat Ozcan <muratkerem@gmail.com>
2023-10-06 08:06:10 -0500

    crud object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -23,3 +23,10 @@ export async function PUT(request: NextRequest, {params: {id}}: Props) {
 
   return NextResponse.json({id: Number(id), name: body.name})
 }
+
+export async function DELETE(request: NextRequest, {params: {id}}: Props) {
+  if (id > 10)
+    return NextResponse.json({error: 'User not found'}, {status: 404})
+
+  return NextResponse.json({})
+}
