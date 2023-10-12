d882e66b19f2dcb49bb4a23246d0609b4016aed0
Murat Ozcan <muratkerem@gmail.com>
2023-10-12 09:36:45 -0500

    correction [ci skip]

--- app/NavBar.cy.tsx
+++ app/NavBar.cy.tsx
@@ -7,7 +7,7 @@ describe('<NavBar />', () => {
 
     cy.getByCy('navbar-home-link')
     cy.getByCy('navbar-users-link')
-    cy.getByCy('navbar-sing-in').click()
+    cy.getByCy('navbar-sign-in').click()
     cy.location('pathname').should('equal', '/api/auth/signin')
   })
 })
