5642ba5a1d4f403e1ceae0b96c2bc72ea4191e65
Murat Ozcan <muratkerem@gmail.com>
2023-10-14 08:01:49 -0500

    testing next-auth  getServerSession

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,18 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-const handler = NextAuth({
+// export the options so we can use them
+// to access the session at the server, at Home component
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-})
+}
+
+const handler = NextAuth(authOptions)
 
 // any GET or POST will get handled by the NextAuth handler
 export {handler as GET, handler as POST}
---
+++ app/pageCore.cy.tsx
@@ -0,0 +1,26 @@
+import HomeCoreComponent from './pageCore'
+import type {Session} from './pageCore'
+
+describe('<Home />', () => {
+  it('should render the user name when there is a session', () => {
+    const session: Session = {
+      user: {
+        name: 'Murat',
+        email: 'murat@gmail.com',
+        image: null,
+      },
+      expires: '3000-01-01T00:00:00.000Z',
+    }
+
+    cy.mount(<HomeCoreComponent session={session} />)
+    cy.contains(`Hello ${session.user.name}`)
+
+    cy.getByCy('home-page-users-link').should('have.attr', 'href', '/users')
+    cy.getByCy('add-to-cart-comp').should('be.visible')
+  })
+
+  it('should only say hello without a session', () => {
+    cy.mount(<HomeCoreComponent session={null} />)
+    cy.get('h1').should('have.text', 'Hello ')
+  })
+})
