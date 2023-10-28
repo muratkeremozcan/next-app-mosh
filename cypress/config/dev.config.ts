b7fbcc5694ebd40fd8145b683f3c7de3fa1549b8
Murat Ozcan <muratkerem@gmail.com>
2023-10-28 08:30:59 -0500

    ignore api route in deployment

--- cypress/config/dev.config.ts
+++ cypress/config/dev.config.ts
@@ -5,6 +5,7 @@ import {baseConfig} from './base.config'
 const specificConfig: Partial<Cypress.PluginConfigOptions> = {
   e2e: {
     baseUrl: 'https://next-app-mosh-cuvk662fv-muratkeremozcan.vercel.app/',
+    excludeSpecPattern: 'cypress/e2e/api/**/*', // not sure how we hit api routes in a deployment
   },
 }
 
